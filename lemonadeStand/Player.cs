a562274fc40cc429e535f99d577f90fb3c28ad66
Tyler Koepke <t.koepke10190@gmail.com>
2019-08-05 09:35:10 -0500

    init

---
+++ lemonadeStand/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lemonadeStand
+{
+
+//member variables (Has A)
+
+//Constructor (Spawner)
+    class Player
+    {
+    }
+
+//Member Methods (Can Do)
+}
